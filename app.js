7a34aee6dd7febdcd915252cc72fc2b21e23f18a
Vishnu Bharathi <vishnubharathi04@gmail.com>
2015-09-21 16:07:36 +0530

    things other than file upload

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 var app = angular.module('wordy', ['angularFileUpload']);
 
+//Utility Factory
 app.factory('utils', function () {
-	var isKeyValueObjInArr = function (arr, key, val) {
+	var getKeyValueObjArrInArr = function(arr , key , val){
 		var filteredArr = arr.filter(function (entry) {
 			return entry[key] === val;
 		});
-		return filteredArr.length > 0;
+		return filteredArr;
+	};
+	var isKeyValueObjInArr = function (arr, key, val) {
+		return getKeyValueObjArrInArr(arr , key , val ).length > 0;
 	};
 	var sortByDecendingOrder = function(a , b ){
 		if(a < b )	return 1 ;
 		else if (a > b ) return -1 ;
 		else return 0;
 	};
+	var cloneArray = function(arr){
+		return arr.slice(0);
+	}
 	return {
+		getKeyValueObjArrInArr : getKeyValueObjArrInArr,
 		isKeyValueObjInArr : isKeyValueObjInArr , 
-		sortByDecendingOrder : sortByDecendingOrder
+		sortByDecendingOrder : sortByDecendingOrder ,
+		cloneArray : cloneArray
 	}
 });
+
+//at.js Factory for creating at.js instance 
 app.factory('atJs' , function(){
 	var create = function(jQueryIdentifier , dataArray , limit ){
 		$(jQueryIdentifier).atwho({
@@ -31,6 +42,8 @@ app.factory('atJs' , function(){
 		create : create 
 	};	
 });
+
+//Text Controller for performing text editor related operations 
 app.controller('TextController', ['$scope' , 'FileUploader' , 'utils', 'atJs' , function ($scope , FileUploader , utils , atJs) {
 	
 	var srcArr = [];
@@ -41,12 +54,14 @@ app.controller('TextController', ['$scope' , 'FileUploader' , 'utils', 'atJs' , 
 		return $('#' + id).caret('position');
 	};
 	
-	 $scope.uploader = new FileUploader();
+	 $scope.uploader = new FileUploader({
+		 url : 'http://localhost:1337/parse'
+	 });
 	 
 	 $scope.$watch('contentText', function (content) {
 		//if no content empty source and populate network cached tokens 
 		if (!content){
-			srcArr = networkTokensCache ;
+			srcArr = utils.cloneArray(networkTokensCache);
 			atJs.create('#inputor' , srcArr , $scope.predictionLimit);
 			return;		
 		}
@@ -59,11 +74,13 @@ app.controller('TextController', ['$scope' , 'FileUploader' , 'utils', 'atJs' , 
 			for(var i = 0 ; i < dataArr.length -1 ; i++){
 				//match the key 
 				if(dataArr[i] === key){
-					if( !utils.isKeyValueObjInArr(srcArr , 'token' , dataArr[i + 1]) ){
+					if(!utils.isKeyValueObjInArr(utils.getKeyValueObjArrInArr(srcArr , 'key' , key ), 'token' , dataArr[i + 1])){
+					    console.log('added key ' + key + ' value ' + dataArr[i+1]);
 						srcArr.push({ key : dataArr[i] , token : dataArr[i+1] , count : 1 });
 					}else{
 						for(var j = 0  ; j < srcArr.length ; j++ ){
 							if( srcArr[j].key === dataArr[i] && srcArr[j].token === dataArr[i+1] ){
+								console.log('count incremented key ' + key + ' value ' + dataArr[i+1]);
 								srcArr[j].count = srcArr[j].count + 1 ;
 								break; 
 							}
