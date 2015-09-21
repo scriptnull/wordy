bf62d27031416ff77c9d425b027e4dc6b3d2d736
Vishnu Bharathi <vishnubharathi04@gmail.com>
2015-09-21 20:12:19 +0530

    added bootstrap and done with frontend

--- app.js
+++ app.js
@@ -49,16 +49,39 @@ app.controller('TextController', ['$scope' , 'utils', 'atJs' , function ($scope 
 	var srcArr = [];
 	var networkTokensCache = [];
 	 $scope.predictionLimit = 3;
+	 $scope.uploadList = [];
+	 $scope.hideUploadButton = false; 
+	 $scope.logList = [];
 	 
+	$scope.onUpload = function(files){
+		$scope.hideUploadButton = true;
+		files[0]['progress'] = 'Uploading file ...';
+		$scope.currentUploadItem = files[0];
+		$scope.logList.push(files[0].name + ' upload started.');
+	};
+	
 	 $scope.onSuccess = function(response){
-		if(response.data){
-			response.data.forEach(function(res){
+		 $scope.hideUploadButton = false ;
+		 $scope.currentUploadItem.progress = "Upload Completed."; 
+		 if(response.data && response.data.srcArr.forEach){
+			response.data.srcArr.forEach(function(res){
 				networkTokensCache.push(res);
 				srcArr.push(res);
 			});
-		}
+		 }
+		 $scope.logList.push(response.data.fileInfo.fileDescriptor.filename + ' uploaded successfully.');
 	};
-	 
+	
+	$scope.onError = function(response){
+		$scope.hideUploadButton = false ;
+		$scope.currentUploadItem.progress = "Something went wrong while uploading.";
+		$scope.logList.push(response.data.fileInfo.fileDescriptor.filename + ' upload failed.');
+	};
+	
+	$scope.onComplete = function(response){
+			
+	};
+	
 	 $scope.$watch('contentText', function (content) {
 		//if no content empty source and populate network cached tokens 
 		if (!content){
@@ -68,8 +91,8 @@ app.controller('TextController', ['$scope' , 'utils', 'atJs' , function ($scope 
 		}
 		//if last char is space 
 		if (content.toString().lastIndexOf(' ') == content.length - 1) {
-			console.log(srcArr);
-			console.log(networkTokensCache);
+			// console.log(srcArr);
+			// console.log(networkTokensCache);
 			var dataArr = content.split(' ').filter(function(val){
 				return val != '';
 			});
